6ef3c3ff0c5f5cbccbb043feb2807866f133a898
bsameer65 <sameerk6500@gmail.com>
2026-08-30 23:27:39 +0200

    updated urls.py

--- project3/urls.py
+++ project3/urls.py
@@ -8,20 +8,34 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("baseline/", views.baseline, name="baseline"),
     path("expert/", views.expert, name="expert"),
+
     path(
         "learning-to-defer/",
         views.learning_to_defer,
         name="learning_to_defer",
     ),
+
     path(
         "active-learning/",
         views.active_learning,
         name="active_learning",
     ),
-    path("compare/", views.compare_results, name="compare_results"),
+
     path(
         "human-expert/",
         views.human_expert,
         name="human_expert",
+    ),
+
+    path(
+        "analysis/",
+        views.advanced_analysis,
+        name="advanced_analysis",
+    ),
+
+    path(
+        "compare/",
+        views.compare_results,
+        name="compare_results",
     ),
 ]
